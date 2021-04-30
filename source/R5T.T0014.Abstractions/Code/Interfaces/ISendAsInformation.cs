72e3525cd6142891fa690e445ca14fe189ba8325
David Coats <david@getsafetycone.com>
2021-04-30 15:07:11 -0700

    Initial project stub. Add R5T.T0014.Abstractions library. Add SendAsInformation abstraction and type.

---
+++ source/R5T.T0014.Abstractions/Code/Interfaces/ISendAsInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using R5T.Aalborg;
+
+
+namespace R5T.T0014
+{
+    /// <summary>
+    /// Representation of information to identifier a sender (based on email).
+    /// </summary>
+    /// <remarks>
+    /// Interface is read-only.
+    /// Favor composition over inheritance for authentication.
+    /// </remarks>
+    public interface ISendAsInformation
+    {
+        IAuthentication Authentication { get; }
+        string EmailAddress { get; }
+        string DisplayName { get; }
+    }
+}
---
+++ source/R5T.T0014/Code/Classes/SendAsInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using R5T.Aalborg;
+
+
+namespace R5T.T0014
+{
+    public class SendAsInformation : ISendAsInformation
+    {
+        public IAuthentication Authentication { get; set; }
+        public string EmailAddress { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
